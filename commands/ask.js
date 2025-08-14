4481a58a036a3f1a52621a8e53378a6d31a43158
mbtaco <121986009+mbtaco@users.noreply.github.com>
2025-08-14 09:51:48 +0200

    Update ask.js

--- commands/ask.js
+++ commands/ask.js
@@ -3,18 +3,17 @@ module.exports = {
   async execute(interaction, db) {
     const question = interaction.options.getString('question');
 
-    // Save to PostgreSQL
     try {
       await db.query(
-        `INSERT INTO messages (message_id, channel_id, author_id, content, timestamp)
-         VALUES ($1, $2, $3, $4, $5)
-         ON CONFLICT (message_id) DO NOTHING`,
+        `INSERT INTO messages (message_id, server_id, channel_id, author_id, content, timestamp)
+         VALUES ($1, $2, $3, $4, $5, $6)`,
         [
-          interaction.id,
-          interaction.channelId,
-          interaction.user.id,
-          question,
-          new Date()
+          interaction.id,           // Discord interaction ID
+          interaction.guildId,      // Server ID
+          interaction.channelId,    // Channel ID
+          interaction.user.id,      // Author ID
+          question,                 // Content
+          new Date()                // Timestamp
         ]
       );
     } catch (err) {
