c60000b54c354cdaffb61a4ef854bfe1120a69a6
mbtaco <121986009+mbtaco@users.noreply.github.com>
2025-08-15 17:53:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ client.on('interactionCreate', async (interaction) => {
                         parts: [{
                             text: getSystemPrompt()
                         }]
-                    };
+                    },
                     history: history
                 });
 
